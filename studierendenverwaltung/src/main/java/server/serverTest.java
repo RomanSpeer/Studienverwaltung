77de5abf437a996e8c4eb77d4e18fff468f889ce
romanspeer <romanspeer@193.22.74.23>
2018-04-30 09:50:52 +0200

    pom.xml verbessert

--- studierendenverwaltung/src/main/java/server/serverTest.java
+++ studierendenverwaltung/src/main/java/server/serverTest.java
@@ -1,8 +1,14 @@
 package server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
 public class serverTest {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 
 	}
