a63381a263476cabd24ce7ff00acd1a5b177f22a
Steffen <steffen.hoeltje@pb.fhdw.de>
2018-05-17 11:38:14 +0200

    Anpassungen erstter HTML Elemente

---
+++ studierendenverwaltung/src/main/java/server/HtmlElements.java
@@ -0,0 +1,197 @@
+package server;
+
+import java.util.List;
+
+import klassen.Benutzer;
+
+public class HtmlElements {
+
+	public static String getNavBarCSS() {
+		return 	/* Navbar container */
+				".navbar {" 
+				+ "	overflow: hidden;" 
+				+ "	background-color: #333;" 
+				+ "	font-family: Arial;"
+				+ "}"
+				/* Links inside the navbar */
+				+ ".navbar a {"
+				+ "	float: left;"
+				+ "	font-size: 16px;"
+				+ "	color: white;"
+				+ "	text-align: center;"
+				+ "	padding: 14px 16px;"
+				+ "	text-decoration: none;"
+				+ "}"
+				/* The dropdown container */
+				+ ".dropdown {"
+				+ "	float: left;"
+				+ "	overflow: hidden;"
+				+ "}"
+				/* Dropdown button */
+				+ ".dropdown .dropbtn {"
+				+ "	font-size: 16px;"
+				+ "	border: none;"
+				+ "	outline: none;"
+				+ "	color: white;"
+				+ "	padding: 14px 16px;"
+				+ "	background-color: inherit;"
+				+ "	font-family: inherit;"
+				+ "	margin: 0;"
+				+ "}"
+				/* Add a red background color to navbar links on hover */
+				+ ".navbar a:hover, .dropdown:hover .dropbtn {"
+				+ "	background-color: red;"
+				+ "}"
+				/* Dropdown content (hidden by default) */
+				+ ".dropdown-content {"
+				+ "	display: none;"
+				+ "	position: absolute;"
+				+ "	background-color: #f9f9f9;"
+				+ "	min-width: 160px;"
+				+ "	box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2)"
+				+ "	z-index: 1;"
+				+ "}"
+				/* Links inside the dropdown */
+				+ ".dropdown-content a {"
+				+ "	float: none;"
+				+ "	color: black;"
+				+ "	padding: 12px 16px;"
+				+ "	text-decoration: none;"
+				+ "	display: block;"
+				+ " text-align: left;"
+				+ "}"
+				/* Add a grey background color to dropdown links on hover */
+				+ ".dropdown-content a:hover {"
+				+ "	background-color: #ddd;"
+				+ "}"
+				/* Show the dropdown menu on hover */
+				+ ".dropdown:hover .dropdown-content {"
+				+ "	display: block;"
+				+ "}";
+	}
+
+	public static String getNavBar() {
+		return "<div class=\"navbar\">"
+				+ "	<a class=\"active\" href=\"#home\">Startseite</a>"
+				+ "	<a href=\"#news\">Notenübersicht</a>"
+				+ "	<div class=\"dropdown\">"
+				+ "		<button class=\"dropbtn\">Klasse"
+				+ "			<i class=\"fa fa-caret-down\"></i>"
+				+ " 	</button>"
+				+ "		<div class=\"dropdown-content\">"
+				+ "			<a href=\"#\">Fächer</a>"
+				+ "			<a href=\"#\">Studierende</a>"
+				+ "		</div>"
+				+ "	</div>"
+				+ "	<a href=\"#about\">Einstellungen</a>"
+				+ "	<a href=\"#news\">Abmelden</a>"
+				+ "</div>";
+	}
+	
+	public static String getLogin(boolean loginStatus) {
+		String fehlerMeldung = "";
+		
+		if(loginStatus==false)
+			fehlerMeldung = "<lable>Benutzername oder Passwort ist falsch</label><br/><br/>";
+			
+		return "<form method=\"post\">"
+				+ "<label>Benutzername: </label>"
+				+ "<input type=\"text\" name=\"user\" length=\"100\""
+				+ "<br/><br/><br/>"
+				+ "<label>Passwort: </label>"
+				+ "<input type=\"password\" name=\"password\" length=\"100\""
+				+ "<br/><br/><br/>"
+				+ fehlerMeldung
+				+ "<input type=\"submit\" value=\"Anmelden\">"
+				+ "</form>";
+	}	
+	
+	public static String getUserTable(List<Object> givenUserList) {
+		
+		//if(!givenUserList.isEmpty()) {
+		if(givenUserList==null) {
+			String tableHead ="<div class=”tbl-content”>"  
+					+"<table id=\"userTable\">"
+					+ "<thead>"
+					+ "	<th>Vorname</th>"
+					+ "	<th>Name</th>"
+				    + "	<th>Kuerzel</th>"
+				    + "	<th>Email</th>"
+				    +""
+				    + "<\thead>";
+			
+			String tableBody = "<tbody>";
+
+//			for(Object tmpObject : givenUserList)
+//			{
+//				Benutzer tmpBenuter = (Benutzer) tmpObject;
+//				tableBody +="<tr>"
+//							+"	<td>"+tmpBenuter.getVorname()+"</td>" 
+//							+"	<td>"+tmpBenuter.getName()+"</td>"
+//							+"	<td>"+tmpBenuter.getKuerzel()+"</td>"
+//							+"	<td>"+tmpBenuter.getPasswort()+"</td>"
+//							+""
+//							+"<\tr>";
+//			}
+			
+			for(int i = 0; i< 100;i++)
+			{
+		
+				tableBody +="<tr>"
+							+"	<td>test</td>" 
+							+"	<td>test</td>"
+							+"	<td>test</td>"
+							+"	<td>test</td>"
+							+""
+							+"</tr>";
+			}
+			
+			tableBody += "</tbody>";
+			
+			String tableEnd= "</table></div>";
+			
+			return tableHead+tableBody+tableEnd;
+		}	
+		
+		return "Keine Benutzer vorhanden";
+	}
+
+	public static String getUserTableCSS() {
+		return "table{"  
+				+"	width:100%;"
+				+" 	cellpadding=”0″;"
+				+"	cellspacing=”0″;"
+				+"	border=”0″;"
+				+"}" 
+				+"th{"  
+				+"	padding: 15px;"  
+				+"	text-align: left;"  
+				+"	vertical-align:middle;"  
+				+"	font-weight: 1000;"  
+				+"	border-bottom: solid 2px rgba(0,0,0,0.5);"  
+				+"	text-transform: uppercase;"  
+				+"}"
+				+"td{"  
+				+"	padding: 15px;"  
+				+"	text-align: left;"  
+				+"	vertical-align:middle;"  
+				+"	font-weight: 300;"  
+				+"	border-bottom: solid 1px rgba(0,0,0,0.2);"  
+				+"	text-transform: uppercase;"  
+				+"}"
+				+".tbl-content{"    
+				+"	height:auto;"  
+				+"	overflow-x:auto;"  
+				+"	margin-top: 0px;"  
+				+"	border: 1px solid rgba(255,255,255,0.9);"  
+				+"	background-color: white;"  
+				+"	border-radius: 5px;"
+				+"	box-shadow: 0px 1px 5px 4px;"
+				+"	rgba(0,0,0,0.2);"  
+				+"}"
+				+".tableHr{"
+				+"	padding: 0px;"
+				+"}";
+		
+	}
+}
--- studierendenverwaltung/src/main/java/server/MainPageServlet.java
+++ studierendenverwaltung/src/main/java/server/MainPageServlet.java
@@ -24,105 +24,17 @@ public class MainPageServlet extends HttpServlet {
 		sb.append("<!DOCTYPE html>");
 		sb.append("<html>");
 		sb.append("  <head>");
-			sb.append("  <style>");
-			/* Navbar container */
-			sb.append(".navbar {");
-		    sb.append(" overflow: hidden;");
-			sb.append("background-color: #333;");
-			sb.append("	  font-family: Arial;");
-			sb.append("	}");
-
-			/* Links inside the navbar */
-			sb.append("	.navbar a {");
-			sb.append("  float: left;");
-		    sb.append(" font-size: 16px;");
-			sb.append("  color: white;");
-			sb.append("  text-align: center;");
-			sb.append("  padding: 14px 16px;");
-			sb.append("  text-decoration: none;");
-			sb.append("}");
-
-			/* The dropdown container */
-			sb.append(".dropdown {");
-			sb.append("  float: left;");
-			sb.append("  overflow: hidden;");
-			sb.append("}");
-
-			/* Dropdown button */
-			sb.append(".dropdown .dropbtn {");
-			sb.append("  font-size: 16px; ");
-			sb.append("  border: none;");
-			sb.append("  outline: none;");
-			sb.append(" color: white;");
-		    sb.append(" padding: 14px 16px;");
-			sb.append(" background-color: inherit;");
-			sb.append(" font-family: inherit;");
-			sb.append("  margin: 0; ");
-			sb.append("}");
-
-			/* Add a red background color to navbar links on hover */
-			sb.append(".navbar a:hover, .dropdown:hover .dropbtn {");
-			sb.append("  background-color: red;");
-			sb.append("}");
-
-			/* Dropdown content (hidden by default) */
-			sb.append(".dropdown-content {");
-			sb.append(" display: none;");
-			sb.append(" position: absolute;");
-			sb.append("background-color: #f9f9f9;");
-		    sb.append(" min-width: 160px;");
-			sb.append(" box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2)");
-			sb.append(" z-index: 1;");
-			sb.append("}");
-
-			/* Links inside the dropdown */
-			sb.append(".dropdown-content a {");
-			sb.append("  float: none;");
-			sb.append("  color: black;");
-			sb.append("  padding: 12px 16px;");
-			sb.append("  text-decoration: none;");
-			sb.append("  display: block;");
-			sb.append("  text-align: left;");
-			sb.append("	}");
-
-			/* Add a grey background color to dropdown links on hover */
-			sb.append(".dropdown-content a:hover {");
-			sb.append("background-color: #ddd;");
-			sb.append("}");
-
-			/* Show the dropdown menu on hover */
-			sb.append(".dropdown:hover .dropdown-content {");
-			sb.append("  display: block;");
-			sb.append("}");
-			
+			sb.append("  <style>");			
+			sb.append(HtmlElements.getNavBarCSS());
+			sb.append(HtmlElements.getUserTableCSS());
 			sb.append("  </style>");
 		sb.append("  </head>");
 		sb.append("  <body>");
 		
-		sb.append("<div class=\"navbar\">\\");
-			sb.append("<a class=\"active\" href=\"#home\">Startseite</a>");
-			sb.append("<a href=\"#news\">Notenübersicht</a>");
-			sb.append("<div class=\"dropdown\">");
-			sb.append("<button class=\"dropbtn\">Klasse ");
-			sb.append("<i class=\"fa fa-caret-down\"></i>");
-			sb.append(" </button>");
-			sb.append("<div class=\"dropdown-content\">");
-			sb.append("<a href=\"#\">Fächer</a>");
-			sb.append(" <a href=\"#\">Studierende</a>");
-			sb.append(" </div>");
-			sb.append(" </div> ");
-			
-		    sb.append("<a href=\"#about\">Einstellungen</a>");
-		    sb.append("<a href=\"#news\">Abmelden</a>");
-			sb.append("</div>");
-		
-//			sb.append("    <form method=\"post\">");
-//			sb.append("      <label>Benutzername: </label>");
-//			sb.append("      <input type=\"text\" name=\"user\" length=\"100\" >");
-//			sb.append("      <br/>");
-//			sb.append("      <br/>");
-//			sb.append("      <input type=\"submit\" value=\"Anmelden\">");
-//			sb.append("    </form>");
+		sb.append(HtmlElements.getNavBar());
+		//sb.append(HtmlElements.getLogin(false));
+		sb.append(HtmlElements.getUserTable(null));
+
 		sb.append("  </body>");
 		sb.append("</html>");
 		return sb;
